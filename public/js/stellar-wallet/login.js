6c2194f569792eb689bfb8f22f3f4b03d572f26e
a3magic3pocket <a3magic3pocket@gmail.com>
2024-07-02 23:50:45 +0900

    Implement user signup.

--- public/js/stellar-wallet/login.js
+++ public/js/stellar-wallet/login.js
@@ -1,5 +1,7 @@
 class Login {
+  rootUrl;
   constructor() {
+    this.rootUrl = "http://localhost:8080";
     const userEmailElem = document.querySelector("#user-email");
     const userPasswordElem = document.querySelector("#user-password");
     if (userEmailElem) {
@@ -10,22 +12,31 @@ class Login {
     }
   }
 
-  async login() {
-    const loginUrl = "http://localhost:8080/auth/login";
-    const userEmail = document.querySelector("#user-email").value;
-    const userPassword = document.querySelector("#user-password").value;
-    if (!userEmail) {
+  #validate(email, password) {
+    if (!email) {
       Swal.fire({
         title: "email을 입력해주세요",
         icon: "error",
       });
-      return;
+      return false;
     }
-    if (!userPassword) {
+    if (!password) {
       Swal.fire({
         title: "password를 입력해주세요",
         icon: "error",
       });
+      return false;
+    }
+
+    return true;
+  }
+
+  async signUp() {
+    const url = `${this.rootUrl}/user`;
+    let userEmail = document.querySelector("#user-email").value;
+    let userPassword = document.querySelector("#user-password").value;
+    const isOk = this.#validate(userEmail, userPassword);
+    if (!isOk) {
       return;
     }
 
@@ -33,9 +44,59 @@ class Login {
       email: userEmail,
       password: userPassword,
     };
-
     try {
-      const resp = await fetch(loginUrl, {
+      const resp = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (resp.ok) {
+        Swal.fire({
+          title: "회원가입 성공!",
+          icon: "success",
+        });
+        return;
+      }
+
+      const respData = await resp.json();
+      const duplicatedMessage = "the email address is already registered";
+      if (respData.message && respData.message === duplicatedMessage) {
+        Swal.fire({
+          title: "이미 가입한 이메일입니다",
+          icon: "error",
+        });
+        return;
+      }
+
+      throw new Error("failed to sing up");
+    } catch (error) {
+      console.error("error", error);
+      Swal.fire({
+        title: "회원가입 실패!",
+        icon: "error",
+      });
+      return;
+    }
+  }
+
+  async login() {
+    const url = `${this.rootUrl}/auth/login`;
+    const userEmail = document.querySelector("#user-email").value;
+    const userPassword = document.querySelector("#user-password").value;
+    const isOk = this.#validate(userEmail, userPassword);
+    if (!isOk) {
+      return;
+    }
+
+    const data = {
+      email: userEmail,
+      password: userPassword,
+    };
+    try {
+      const resp = await fetch(url, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -47,9 +108,10 @@ class Login {
         window.location.href = "/view/main";
         return;
       }
+
+      throw new Error("failed to login");
     } catch (error) {
       console.error("error", error);
-    } finally {
       Swal.fire({
         title: "로그인 실패!",
         icon: "error",
