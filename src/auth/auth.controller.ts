19e01beecddc1d81fca29dc23de4763e31c8e897
a3magic3pocket <a3magic3pocket@gmail.com>
2024-07-02 23:56:36 +0900

    Implement logout.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,6 +5,7 @@ import { Request } from "express";
 import { IAuthSession } from "./interface/auth-session.interface";
 import { User } from "src/user/user.entity";
 import { ISimpleSuccessRespDto } from "./global/dto/interface/simple-success-resp-dto.interface";
+import { LoginGaurd } from "./login.guard";
 
 @Controller("/auth")
 export class AuthController {
@@ -12,6 +13,18 @@ export class AuthController {
   @UseGuards(AuthGuard("local"))
   async login(@Req() req: Request, @Session() session: IAuthSession) {
     session.user = req.user as User;
+
+    const resp: ISimpleSuccessRespDto = {
+      message: "success",
+    };
+
+    return resp;
+  }
+
+  @Post("/logout")
+  @UseGuards(LoginGaurd)
+  async logout(@Session() session: IAuthSession) {
+    session.destroy(() => {});
 
     const resp: ISimpleSuccessRespDto = {
       message: "success",
